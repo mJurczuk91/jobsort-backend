45ab24e539e8897377a5f695a277df5c46fd53c0
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-04-04 13:36:43 +0200

    feature: basic queries, auth, and error handling

---
+++ db.js
@@ -0,0 +1,150 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+});
+
+const getOffers = (request, response) => {
+    pool.query('SELECT * FROM job_offers ORDER BY offer_valid_date ASC', (error, results) => {
+        if (error) {
+            throw new Error({
+                code: 500,
+                message: 'something went wrong',
+            })
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getOfferByLink = (request, response) => {
+    const { link } = { ...request.body };
+
+    if (!link) {
+        throw new Error({
+            code: 400,
+            message: 'offer link is required'
+        })
+    }
+
+    pool.query('SELECT * FROM job_offers WHERE job_offer_link = $1', [link], (error, results) => {
+        if (error) {
+            throw new Error({
+                code: 500,
+                message: 'something went wrong',
+            })
+        }
+        else {
+            response.status(200).json(results.rows);
+        }
+    })
+
+}
+
+const createOffer = (request, response) => {
+    const {
+        link,
+    } = { ...request.body };
+
+    if (!link) throw new Error({
+        code: 400,
+        message: 'offer link is required'
+    });
+
+    pool.query('SELECT * FROM job_offers WHERE job_offer_link = $1', [link], (error, results) => {
+        if (error) {
+            throw new Error({
+                code: 500,
+                message: 'something went wrong',
+            })
+        }
+        if (results && results.length > 0) {
+            throw new Error({
+                code: 400,
+                message: 'record with that link already exists',
+            })
+        }
+        const query = createOfferQueryConstructor(request.body);
+        pool.query(query, (error, results) => {
+            if (error) {
+                console.log('error', error);
+            }
+            else {
+                response.status(201).send('Created record')
+            }
+        })
+    })
+}
+
+const createOfferQueryConstructor = (offer) => {
+    const fields = [];
+    const values = [];
+    const {
+        link,
+        title,
+        description,
+        technologies,
+        responsibilities,
+        requirements,
+        optionalRequirements,
+        offerValidDate,
+        isJuniorFriendly,
+        noExperienceRequired,
+    } = { ...offer };
+
+    if (link) {
+        fields.push('offer_link');
+        values.push(`'${link}'`);
+    }
+    if (title) {
+        fields.push('title');
+        values.push(`'${title}'`);
+    }
+    if (description) {
+        fields.push('description');
+        values.push(`'${description}'`);
+    }
+    if (technologies && technologies.length > 0) {
+        fields.push('technologies');
+        values.push(`ARRAY [${[technologies.map(row => `'${row}'`)]}]`);
+    }
+    if (responsibilities && responsibilities.length > 0) {
+        fields.push('responsibilities');
+        values.push(`ARRAY [${[responsibilities.map(row => `'${row}'`)]}]`);
+    }
+    if (requirements && requirements.length > 0) {
+        fields.push('requirements');
+        values.push(`ARRAY [${[requirements.map(row => `'${row}'`)]}]`);
+    }
+    if (optionalRequirements && optionalRequirements.length > 0) {
+        fields.push('optional_requirements');
+        values.push(`ARRAY [${[optionalRequirements.map(row => `'${row}'`)]}]`);
+    }
+    if (isJuniorFriendly) {
+        fields.push('is_junior_friendly');
+        values.push(isJuniorFriendly);
+    }
+    if (noExperienceRequired) {
+        fields.push('no_experience_required');
+        values.push(noExperienceRequired);
+    }
+    if (offerValidDate) {
+        fields.push('offer_valid_date');
+        values.push(offerValidDate);
+    }
+
+    const queryString = `
+    INSERT INTO job_offers(${[...fields]})
+    VALUES (${[...values]});`
+
+    return queryString;
+}
+
+module.exports = {
+    getOfferByLink,
+    createOffer,
+    getOffers,
+}
--- server.js
+++ server.js
@@ -1,12 +1,52 @@
-import { configDotenv } from "dotenv";
-import { Pool } from "pg";
-
+const configDotenv = require('dotenv').configDotenv;
 configDotenv();
 
-const pool = new Pool({
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
+const {getOfferByLink, createOffer, getOffers} = require('./db');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const port = 3010;
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+app.use((req, res, next) => {
+    const key = req.get('key');
+    const key_local = process.env.AUTH_KEY;
+    if (!key_local) {
+        throw new Error(JSON.stringify({
+            code: 500,
+            message: 'Local api key not defined',
+        }));
+    };
+    if (!key || key !== key_local) {
+        throw new Error(JSON.stringify({
+            code: 403,
+            message: 'Auth passport mismatch',
+        }));
+    };
+    next();
+});
+
+app.use((err, req, res, next) => {
+    try {
+        const { message, code } = { ...JSON.parse(err.message) }
+        console.error('Error:', message);
+        res.status(code).send(message);
+    } catch (e) {
+        console.log('ERROR HANDLING MIDDLEWARE');
+        console.log(JSON.parse(e));
+        res.status(500).send('Something went wrong')
+    }
+});
+
+
+app.get('/offer', getOfferByLink);
+app.get('/offers', getOffers);
+app.post('/offer', createOffer);
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
 })
